9db30065f022f6771635b8b597cae174c12405d9
sahilduttwebdesigner900 <sahilwebdesigner900@gmail.com>
2026-03-12 18:09:36 +0530

    fix: folder name case

--- Components/awards-section.tsx
+++ Components/awards-section.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { motion } from "framer-motion"
-import { SectionHeader } from "@/Components/section-header"
+import { SectionHeader } from "@/components/section-header"
 import Image from "next/image"
 
 const techStack = [
