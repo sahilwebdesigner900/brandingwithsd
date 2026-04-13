62c5450a31c158cfba3f5621f82d0ad8f09e6a57
sahilduttwebdesigner900 <sahilwebdesigner900@gmail.com>
2026-04-13 18:27:35 +0530

    ignore type script

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+// next.config.js
+module.exports = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
 
 export default nextConfig;
+
